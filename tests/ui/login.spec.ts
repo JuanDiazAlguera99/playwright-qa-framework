603a1dca534873d6234207bd99ecba111183acdb
JUAN DIAZ <juancarlosdiazalguera@gmail.com>
2026-07-22 14:51:21 -0600

    change test cases structure

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/loginPage';
-import { environments } from './../../config/environment';
+import { environments } from '../../config/environment';
 
 const orangeHrm = environments.qa;
 
@@ -8,20 +8,32 @@ test.describe('Login Tests', () => {
   test('User should login successfully', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
-    await loginPage.goToLoginPage();
+    await test.step('Given the user is on the login page', async () => {
+      await loginPage.goToLoginPage();
+    });
 
-    await loginPage.login(orangeHrm.username, orangeHrm.password);
+    await test.step('When the user enters valid credentials', async () => {
+      await loginPage.login(orangeHrm.username, orangeHrm.password);
+    });
 
-    await loginPage.expectLoginSuccessful();
+    await test.step('Then the user should login successfully', async () => {
+      await loginPage.expectLoginSuccessful();
+    });
   });
 
   test('User should not login with invalid credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
-    await loginPage.goToLoginPage();
+    await test.step('Given the user is on the login page', async () => {
+      await loginPage.goToLoginPage();
+    });
 
-    await loginPage.login('InvalidUser', 'InvalidPassword');
+    await test.step('When the user enters invalid credentials', async () => {
+      await loginPage.login('InvalidUser', 'InvalidPassword');
+    });
 
-    await loginPage.expectLoginError();
+    await test.step('Then an error message should be displayed', async () => {
+      await loginPage.expectLoginError();
+    });
   });
 });
